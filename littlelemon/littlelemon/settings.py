bbf6c0502252621f1ef20c82a2643f9ec6d0d9c8
hosseinhtt <hossein.taybi@yahoo.com>
2023-05-09 11:17:33 +0430

    add menu and single menu view and corresponding serializers

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # my app
     'restaurant',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('sayhello', views.say_hello, name='sayHello'),
     path('', views.index, name='index'),
+    path('menu/', views.MenuItemsView.as_view(), name='menu'),
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view(), name='single-menu'),
 ]
