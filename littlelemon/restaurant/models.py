dc3b5b9bdf11dcb70e8b50cf343f69a8b5687d94
hosseinhtt <hossein.taybi@yahoo.com>
2023-05-09 09:32:25 +0430

    insert def str to models to show name of objects at the admin panel

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -8,8 +8,14 @@ class Booking(models.Model):
     No_of_guests = models.IntegerField()
     BookingData = models.DateField()
 
+    def __str__(self) -> str:
+        return self.Name
+
 
 class Menu(models.Model):
     Title = models.CharField(max_length=255)
     Price = models.DecimalField(max_digits=10, decimal_places=2)
     Inventory = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.Title
